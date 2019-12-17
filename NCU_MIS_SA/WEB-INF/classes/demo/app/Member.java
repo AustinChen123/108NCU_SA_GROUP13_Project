eb5c4dd3b0afcd1576bcda5849752950116f52c1
Chia-Yu0902 <ynn984910@gmail.com>
2019-12-17 22:33:38 +0800

    修改member

--- NCU_MIS_SA/WEB-INF/classes/demo/app/Member.java
+++ NCU_MIS_SA/WEB-INF/classes/demo/app/Member.java
@@ -1,217 +1,52 @@
-package ncu.im3069.demo.app;
-
-import org.json.*;
-import java.util.Calendar;
-
-// TODO: Auto-generated Javadoc
-/**
- * <p>
- * The Class Member
- * Member類別（class）具有會員所需要之屬性與方法，並且儲存與會員相關之商業判斷邏輯<br>
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @since 1.0.0
- */
-
-public class Member {
-    
-    /** id，會員編號 */
-    private int id;
-    
-    /** email，會員電子郵件信箱 */
-    private String email;
-    
-    /** name，會員姓名 */
-    private String name;
-    
-    /** password，會員密碼 */
-    private String password;
-    
-    /** login_times，更新時間的分鐘數 */
-    private int login_times;
-    
-    /** status，會員之組別 */
-    private String status;
-    
-    /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
-    private MemberHelper mh =  MemberHelper.getHelper();
-    
     /**
-     * 實例化（Instantiates）一個新的（new）Member物件<br>
-     * 採用多載（overload）方法進行，此建構子用於建立會員資料時，產生一名新的會員
+     * 檢查該名會員是否登入成功
      *
-     * @param email 會員電子信箱
-     * @param password 會員密碼
-     * @param name 會員姓名
+     * @param m 一名會員之Member物件
+     * @return boolean 若成功登入回傳True，若失敗則回傳False
      */
-    public Member(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        update();
-    }
-
-    /**
-     * 實例化（Instantiates）一個新的（new）Member物件<br>
-     * 採用多載（overload）方法進行，此建構子用於更新會員資料時，產生一名會員同時需要去資料庫檢索原有更新時間分鐘數與會員組別
-     * 
-     * @param id 會員編號
-     * @param email 會員電子信箱
-     * @param password 會員密碼
-     * @param name 會員姓名
-     */
-    public Member(int id, String email, String password, String name) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        /** 取回原有資料庫內該名會員之更新時間分鐘數與組別 */
-        getLoginTimesStatus();
-        /** 計算會員之組別 */
-        calcAccName();
-    }
-    
-    /**
-     * 實例化（Instantiates）一個新的（new）Member物件<br>
-     * 採用多載（overload）方法進行，此建構子用於查詢會員資料時，將每一筆資料新增為一個會員物件
-     *
-     * @param id 會員編號
-     * @param email 會員電子信箱
-     * @param password 會員密碼
-     * @param name 會員姓名
-     * @param login_times 更新時間的分鐘數
-     * @param status the 會員之組別
-     */
-    public Member(int id, String email, String password, String name, int login_times, String status) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.login_times = login_times;
-        this.status = status;
-    }
-    
-    /**
-     * 取得會員之編號
-     *
-     * @return the id 回傳會員編號
-     */
-    public int getID() {
-        return this.id;
-    }
-
-    /**
-     * 取得會員之電子郵件信箱
-     *
-     * @return the email 回傳會員電子郵件信箱
-     */
-    public String getEmail() {
-        return this.email;
-    }
-    
-    /**
-     * 取得會員之姓名
-     *
-     * @return the name 回傳會員姓名
-     */
-    public String getName() {
-        return this.name;
-    }
-
-    /**
-     * 取得會員之密碼
-     *
-     * @return the password 回傳會員密碼
-     */
-    public String getPassword() {
-        return this.password;
-    }
-    
-    /**
-     * 取得更新資料時間之分鐘數
-     *
-     * @return the login times 回傳更新資料時間之分鐘數
-     */
-    public int getLoginTimes() {
-        return this.login_times;
-    }
-    
-    /**
-     * 取得會員資料之會員組別
-     *
-     * @return the status 回傳會員組別
-     */
-    public String getStatus() {
-        return this.status;
-    }
-    
-    /**
-     * 更新會員資料
-     *
-     * @return the JSON object 回傳SQL更新之結果與相關封裝之資料
-     */
-    public JSONObject update() {
-        /** 新建一個JSONObject用以儲存更新後之資料 */
-        JSONObject data = new JSONObject();
-        /** 取得更新資料時間（即現在之時間）之分鐘數 */
-        Calendar calendar = Calendar.getInstance();
-        this.login_times = calendar.get(Calendar.MINUTE);
-        /** 計算帳戶所屬之組別 */
-        calcAccName();
+    public boolean checkLogin(Member m){
+        /** 紀錄SQL總行數，若為「-1」代表資料庫檢索尚未完成 */
+        int row = -1;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
         
-        /** 檢查該名會員是否已經在資料庫 */
-        if(this.id != 0) {
-            /** 若有則將目前更新後之資料更新至資料庫中 */
-            mh.updateLoginTimes(this);
-            /** 透過MemberHelper物件，更新目前之會員資料置資料庫中 */
-            data = mh.update(this);
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT count(*) FROM `sa_project`.`members` WHERE `email` = ? and `password` = ? and `status = 1`";
+            
+            /** 取得所需之參數 */
+            String email = m.getEmail();
+            String password = m.getPassword();
+            String status = m.getStatus();
+             
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, email);
+            pres.setString(2, password);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 讓指標移往最後一列，取得目前有幾行在資料庫內 */
+            rs.next();
+            row = rs.getInt("count(*)");
+            System.out.print(row);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
         }
         
-        return data;
+        /** 
+         * 判斷是否已經有一筆該電子郵件信箱之資料
+         * 若無一筆則回傳False，否則回傳True 
+         */
+        return (row == 1) ? true : false;
     }
-    
-    /**
-     * 取得該名會員所有資料
-     *
-     * @return the data 取得該名會員之所有資料並封裝於JSONObject物件內
-     */
-    public JSONObject getData() {
-        /** 透過JSONObject將該名會員所需之資料全部進行封裝*/ 
-        JSONObject jso = new JSONObject();
-        jso.put("id", getID());
-        jso.put("name", getName());
-        jso.put("email", getEmail());
-        jso.put("password", getPassword());
-        jso.put("login_times", getLoginTimes());
-        jso.put("status", getStatus());
-        
-        return jso;
-    }
-    
-    /**
-     * 取得資料庫內之更新資料時間分鐘數與會員組別
-     *
-     */
-    private void getLoginTimesStatus() {
-        /** 透過MemberHelper物件，取得儲存於資料庫的更新時間分鐘數與會員組別 */
-        JSONObject data = mh.getLoginTimesStatus(this);
-        /** 將資料庫所儲存該名會員之相關資料指派至Member物件之屬性 */
-        this.login_times = data.getInt("login_times");
-        this.status = data.getString("status");
-    }
-    
-    /**
-     * 計算會員之組別<br>
-     * 若為偶數則為「偶數會員」，若為奇數則為「奇數會員」
-     */
-    private void calcAccName() {
-        /** 計算目前分鐘數為偶數或奇數 */
-        String curr_status = (this.login_times % 2 == 0) ? "偶數會員" : "奇數會員";
-        /** 將新的會員組別指派至Member之屬性 */
-        this.status = curr_status;
-        /** 檢查該名會員是否已經在資料庫，若有則透過MemberHelper物件，更新目前之組別狀態 */
-        if(this.id != 0) mh.updateStatus(this, curr_status);
-    }
-}
