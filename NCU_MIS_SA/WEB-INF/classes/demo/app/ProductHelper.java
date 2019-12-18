e1f48bd99090af8d73fe49bb365b789b60d31730
g3190220 <g3190220@gmail.com>
2019-12-18 17:08:49 +0800

    Update ProductHelper.java
    新增商品、修改商品和刪除商品已完成(新增商品可能會用到cookie要再研究一下QQ

--- NCU_MIS_SA/WEB-INF/classes/demo/app/ProductHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/demo/app/ProductHelper.java
@@ -1,6 +1,8 @@
 package ncu.im3069.demo.app;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 
 import org.json.*;
 
@@ -24,8 +26,8 @@ public class ProductHelper {
     }
     
     public JSONObject getAll() {
-        /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
-    	Product p = null;
+        /** 新建一個 Product 物件之 p變數，用於紀錄每一位查詢回之商品資料 */
+    	Product p = null; 
         /** 用於儲存所有檢索回之商品，以JSONArray方式儲存 */
         JSONArray jsa = new JSONArray();
         /** 記錄實際執行之SQL指令 */
@@ -70,7 +72,7 @@ public class ProductHelper {
                 String image = rs.getString("image");
                 Boolean verification_status=rs.getBoolean("verification_status");
                 
-                /** 將每一筆商品資料產生一名新Product物件 */
+                /** 將每一筆商品資料產生一個新Product物件 */
                 p = new Product(product_id,seller_name,seller_email,seller_fb, name,classificaion,product_status, price,product_overview, image,verification_status);
                 /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
                 jsa.put(p.getData());
@@ -105,7 +107,7 @@ public class ProductHelper {
 
     
     public Product getById(String id) {
-        /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
+        /** 新建一個 Product 物件之 p 變數，用於紀錄每一位查詢回之商品資料 */
         Product p = null;
         /** 記錄實際執行之SQL指令 */
         String exexcute_sql = "";
@@ -143,7 +145,7 @@ public class ProductHelper {
                 String image = rs.getString("image");
                 Boolean verification_status=rs.getBoolean("verification_status");
                 
-                /** 將每一筆商品資料產生一名新Product物件 */
+                /** 將每一筆商品資料產生一個新Product物件 */
                 p = new Product(product_id,seller_name,seller_email,seller_fb, name,classificaion,product_status, price,product_overview, image,verification_status);
             }
 
@@ -162,7 +164,7 @@ public class ProductHelper {
     }
     
     public Product getByName(String name) {
-        /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
+        /** 新建一個 Product 物件之 p 變數，用於紀錄每一位查詢回之商品資料 */
         Product p = null;
         /** 記錄實際執行之SQL指令 */
         String exexcute_sql = "";
@@ -200,7 +202,7 @@ public class ProductHelper {
                 String image = rs.getString("image");
                 Boolean verification_status=rs.getBoolean("verification_status");
                 
-                /** 將每一筆商品資料產生一名新Product物件 */
+                /** 將每一筆商品資料產生一個新Product物件 */
                 p = new Product(product_id,seller_name,seller_email,seller_fb, name,classificaion,product_status, price,product_overview, image,verification_status);
             }
 
@@ -219,7 +221,7 @@ public class ProductHelper {
     }
     
     public Product getByClassification(String classification) {
-        /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
+        /** 新建一個 Product 物件之 p 變數，用於紀錄每一位查詢回之商品資料 */
         Product p = null;
         /** 記錄實際執行之SQL指令 */
         String exexcute_sql = "";
@@ -257,7 +259,7 @@ public class ProductHelper {
                 String image = rs.getString("image");
                 Boolean verification_status=rs.getBoolean("verification_status");
                 
-                /** 將每一筆商品資料產生一名新Product物件 */
+                /** 將每一筆商品資料產生一個新Product物件 */
                 p = new Product(product_id,seller_name,seller_email,seller_fb, name,classificaion,product_status, price,product_overview, image,verification_status);
             }
 
@@ -273,5 +275,228 @@ public class ProductHelper {
         }
 
         return p;
+    }
+    public JSONObject create(Product p) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            /** member_id從cookie取*/
+            String sql = "INSERT INTO `sa_project`.`products`(`member_id`,`seller_name`, `seller_email`,`seller_fb`, `name`, `classification`, `product_status`,`price`,‵`product_overview`,`image`)"
+                    + " VALUES(?, (SELECT  `name` = ? FROM members WHERE members.id = member_id), (SELECT  `email` = ? FROM members WHERE members.id = member_id), (SELECT  `fb_link` = ? FROM members WHERE members.id = member_id), ?, ?, ?, ?, ?, ?)";
+            
+            /** 取得所需之參數 */
+            int member_id = p.getInt();
+            String seller_name=p.getString();
+            String seller_email=p.getString();
+            String seller_fb=p.getString();
+            String name = p.getString();
+            String classificaion=p.getString();
+            Boolean product_status=p.getBoolean();
+            float price = p.getFloat();
+            String product_overview = p.getString();
+            String image = p.getString();
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, member_id);
+            pres.setString(2, seller_name);
+            pres.setString(3, seller_email);
+            pres.setString(4, seller_fb);
+            pres.setString(5, name);
+            pres.setString(6, classificaion);
+            pres.setBoolean(7, product_status);
+            pres.setFloat(8, price);
+            pres.setString(9, product_overview);
+            pres.setString(10, image);
+            
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+            
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    /**
+     * 更新商品資料
+     *
+     * @param p Product物件
+     * @return the JSONObject 回傳SQL指令執行結果與執行之資料
+     */
+    public JSONObject update(Product p) {
+        /** 紀錄回傳之資料 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "Update `sa_project`.`products` SET `name` = ? , `classification`= ? , `` = ? , `product_status` = ?, `price` = ?, `product_overview` = ?`image` = ? WHERE `id` = ?";
+            
+            /** 取得所需之參數 */
+            
+            int product_id = p.getInt();
+            String name = p.getString();
+            String classificaion=p.getString();
+            Boolean product_status=p.getBoolean();
+            float price = p.getFloat();
+            String product_overview = p.getString();
+            String image = p.getString();
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, name);
+            pres.setString(2, classificaion);
+            pres.setBoolean(3, product_status);
+            pres.setFloat(4, price);
+            pres.setString(5, product_overview);
+            pres.setString(6, image);
+            pres.setInt(7, product_id);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    /**
+     * 更新商品之審核狀態
+     *
+     * @param p Product物件
+     * @param verification_status 商品審核狀態（Boolean）
+     */
+    public void updateStatus(Product p) {      
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            /** 設成1，商品已由管理員審核 */
+            String sql = "Update `sa_project`.`products` SET `verification_status` = 1 WHERE `id` = ?";
+            /** 取得會員編號 */
+            int id = p.getID();
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行更新之SQL指令 */
+            pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+    }
+    /**
+     * 更新商品之上架狀態
+     *
+     * @param p Product物件
+     * @param on_shelf 商品上架狀態（Boolean）
+     */
+    public void updateOn_Shelf(Product p) {      
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            /** 設成1，商品已下架 */
+            String sql = "Update `sa_project`.`products` SET `on_shelf` = 1 WHERE `id` = ?";
+            /** 取得會員編號 */
+            int id = p.getID();
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行更新之SQL指令 */
+            pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
     }
 }
